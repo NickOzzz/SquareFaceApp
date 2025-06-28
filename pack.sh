246f74eb8db37d55be836297fc9c91d3baeb4b79
NickOzzz <ozzziwus@gmail.com>
2025-06-28 08:29:54 +0200

    adjustments to packing and readme clarification

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 echo "THIS IS SQUAREFACE PACKAGES SCRIPT (Valid version of PyInstaller is required!!!)"
 
-pyinstaller --windowed --onefile --paths $PATH_TO_VENV SquareFace.py
+pyinstaller --windowed --noconsole SquareFace.py
